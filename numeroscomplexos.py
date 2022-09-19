497c52794a6024ab61654ccd1009580a5f71036c
Vanessa Montenegro <110609554+VanessaMontenegroS@users.noreply.github.com>
2022-09-18 21:33:54 -0300

    Update numeroscomplexos.py

--- numeroscomplexos.py
+++ numeroscomplexos.py
@@ -1,51 +1,49 @@
 #Crie um tipo abstrato de dado (TAD) para manipular números complexos na linguagem Python.
 #O método deve:
-
 #- calcular três números complexos;
 #- realizar todas as operações básicas;
 #- e imprimir as propriedades real e img do números.
 
-from abc import ABC, abstractmethod
+class numcomplexo:
+    def __init__(self,com1,com2,com3):
+        self.n1=com1
+        self.n2=com2
+        self.n3=com3
+
+    def somar(self):
+        soma=self.n1+self.n2+self.n3
+        print(soma)
+
+    def subtrair(self):
+        subtracao=self.n1-self.n2-self.n3
+        print(subtracao)
+
+    def multiplicar(self):
+        multiplicacao=self.n1*self.n2*self.n3
+        print(multiplicacao)
+    
+    def dividir(self):
+        divisao=self.n1/self.n2/self.n3
+        print(divisao)
+
+a=float(input("Que número será a parte real do 1º número complexo? "))
+b=float(input("Que número será a parte imaginária do 1º número complexo? "))
+c=float(input("Que número será a parte real do 2º número complexo? "))
+d=float(input("Que número será a parte imaginária do 2º número complexo? "))
+e=float(input("Que número será a parte real do 3º número complexo? "))
+f=float(input("Que número será a parte imaginária do 3º número complexo? "))
 
 
-class NumerosComplexos:
+com1=complex(a,b)
+com2=complex(c,d)
+com3=complex(e,f)
 
-    def __init__(self, v1, v2, v3, v4, v5, v6):
-
-        self.valor1 = v1
-        self.valor2 = v2
-        self.valor3 = v3
-        self.valor4 = v4
-        self.valor5 = v5
-        self.valor6 = v6
-
-    def Soma(self):
-
-        self.resultado1 = (self.valor1) + (self.valor3) + (self.valor5)
-        self.resultado2 = (self.valor2) + (self.valor4) + (self.valor6)
-
-
-v1 = float(input('Insira o numero real da equação Z1\n'))
-v2 = float(input('Insira o numero imaginário da equação Z1\n'))
-
-v3 = float(input('Insira o numero real da equação Z2\n'))
-v4 = float(input('Insira o numero imaginário da equação Z2\n'))
-
-v5 = float(input('Insira o numero real da equação Z3\n'))
-v6 = float(input('Insira o numero imaginário da equação Z3\n'))
-print(' ')
-
-a = NumerosComplexos(v1 , v2, v3, v4, v5, v6)
-
-a.Soma()
-print(f'Parte real da Z1 =', a.valor1)
-print(f'Parte imaginária Z1 =', a.valor2,'i\n')
-print(f'Parte real Z2 =', a.valor3)
-print(f'Parte imaginária Z2 =', a.valor4,'i\n')
-print(f'Parte real Z3 =', a.valor5)
-print(f'Parte imaginária Z3 =', a.valor6,'i\n')
-
-print(f'Somando as equações:', '(Z1 =',v1, '+', v2,'i)', ';', '(Z2 =',v3, '+', v4,'i)', 'e' , '(Z3 =',v5,'+',v6,'i)',':\n')
-print(f'A soma da parte real do número complexo é =', a.resultado1, '.\n')
-print(f'A soma da parte imaginaria do número complexo é =', a.resultado2, '.\n')
-print(f'Equação final: (Z =', a.resultado1, '+', a.resultado2,'i) \n')
+resultado=numcomplexo(com1, com2, com3)
+print("O resultado da soma é: ")
+print(resultado.somar())
+print("O resultado da subtração é: ")
+print(resultado.subtrair())
+print("O resultado da multiplicação é: ")
+print(resultado.multiplicar())
+print("O resultado da divisão é: ")
+print(resultado.dividir())
